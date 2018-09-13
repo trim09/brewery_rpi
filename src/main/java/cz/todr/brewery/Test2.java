536820cdd6924101c0851b093f832e84408677c2
Trimo <trim@atlas.cz>
2018-09-13 21:42:43 +0200

    added automatic brewing process skeleton

--- src/main/java/cz/todr/brewery/Test2.java
+++ src/main/java/cz/todr/brewery/Test2.java
@@ -1,71 +1,86 @@
 package cz.todr.brewery;
 
+import com.google.common.collect.ImmutableList;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Mono;
 
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.RejectedExecutionHandler;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.function.Consumer;
 
 @Slf4j
 public class Test2 {
-    ExecutorService executor = Executors.newSingleThreadExecutor(r -> {
-        Thread t = new Thread(r, "Executor1");
-        t.setDaemon(true);
-        return t;
-    });
+
+    @Data
+    @AllArgsConstructor
+    static class ShoppingCart {
+        int id;
+        int userid;
+        List<Long> products;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Product {
+        long id;
+        String text;
+        int price;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class User {
+        long id;
+        String name;
+    }
+
+    Mono<ShoppingCart> getCart(int id) {
+        if (id == 123) {
+            return Mono.just(new ShoppingCart(id, 7337, ImmutableList.of(444L, 555L)));
+        } else {
+            return Mono.empty();
+        }
+    }
+
+    Mono<Product> getProduct(long id) {
+        if (id == 444L) {
+            return Mono.just(new Product(id, "444 mys", 450));
+        } else if (id == 555L) {
+            return Mono.just(new Product(id, "555 klavesnice", 1280));
+        } else {
+            return Mono.empty();
+        }
+    }
+
+    Mono<User> getUser(int id) {
+        if (id == 7337) {
+            return Mono.just(new User(id, "Chuck Norris"));
+        } else {
+            return Mono.empty();
+        }
+    }
+
+    Flux<Product> getProducts(List<Long> ids) {
+        return ids.stream().map(id -> /* TODO <------------------ */)
+    }
 
     private void run() {
-        val flux = Flux.range(1, 20)
-                .delayElements(Duration.of(100, ChronoUnit.MILLIS))
-                .cache(5);
+        //vstup ID kosiku
+        //1. vyhledani kosiku, ktere obsahuje ID uzivatele, list ID produktu
+        //2. dohledani produktu
+        //3. dohledani uzivatele
+        //4. vratit celkovou castku za produkty a pokud je uzivatel == "karel" tak dat 50% slevu
 
-        sleep(1500);
-        System.out.println("consumer1 ");
-        flux.subscribe(value -> {
-            print(value, "Consumed1");
-            //sleep(1500 );
-            //print(value, "Consumed1 end");
-        });
+        //val flux = Flux.range(1, 2);
+        val mono = Mono.just(456);
 
+        mono.map(id -> {
+            ShoppingCart cart = getCart(id);
 
+        })
 
-        System.out.println("consumer2 ");
-        flux.subscribe(value -> {
-            print(value, "Consumed2");
-            //sleep(1500 );
-            //print(value, "Consumed1 end");
-        });
-//        System.out.println("consumer1 ");
-//        flux.subscribe(num -> {
-//            print(num, "Consumed1 start");
-//            sleep(1500 );
-//            print(num, "Consumed1 end");
-//        });
-//        System.out.println("consumer2 ");
-//        flux.subscribe(num -> {
-//            print(num, "Consumed2 start");
-//            sleep(1500 );
-//            print(num, "Consumed2 end");
-//        });
     }
 
     private static void sleep(int millis) {
